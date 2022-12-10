61feec84fcdedb9f08cb8faf529709a68f6ca47a
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-09 19:08:26 -0800

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,19 @@
  */
 int main(void)
 {
-	unsigned int i = '0';
+	unsigned char a = '0';
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-	putchar(i);
+	putchar(a);
 	a++;
 	}
-	i = '1';
+	a = '1';
 	for (i = 0; i < 6; i++)
 	{
-	putchar('0' + i);
-	i++;
+	putchar('0' + a);
+	a++;
 	}
 	putchar('\n');
 	return (0);
