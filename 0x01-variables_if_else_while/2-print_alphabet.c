ffc372bdee1689e9d5c1354bf00b39439c95d9ee
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-09 17:53:20 -0800

    sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Represent Entry poin to the program
+ * Return: 0 to stop the program
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
