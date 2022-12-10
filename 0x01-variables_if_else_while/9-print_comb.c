daa0375e6cf5f8a5e959a44672e74fcb17b8cc2d
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-09 18:52:37 -0800

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - represent Entry poin
+ * Return: 0 for sucess
+ */
+int main(void)
+{
+	unsigned char a = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(a);
+	a++;
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
+	putchar('0' + a);
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
