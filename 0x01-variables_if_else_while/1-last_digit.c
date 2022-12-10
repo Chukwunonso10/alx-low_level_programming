026f8dc59618e03b0e13fcb355b5ea6d5caa1339
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-09 17:41:54 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Determine if the last digit of a random number is greater than or less than 5 or is zero..
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5 or is zero..
  * Return: 0 sucess
  */
 int main(void)
