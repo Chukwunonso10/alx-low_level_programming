c999f4a0e97af8d24bd1843833d5ccd71f685b23
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-13 12:30:55 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Entry point of the programm
+ *Return: 0 for sucess
+ */
+void print_alphabet_x10(void)
+{
+	int a, co;
+	co = 0;
+	while (co < 10)
+	{
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	_putchar(a);
+	}
+	co++;
+	putchar('\n');}
+}
