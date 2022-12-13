3cdd2fa55234ef1a0b2e9967de99b8d1b43411fb
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-13 08:11:18 -0800

    sucess

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 /**
- * main - Entry point of the program
+ * main - Entry point of the programm
  * Return: 0 for sucess
  */
 int main(void)
 {
-	puts("_putchar");
+	int char;
+
+	putchar(_);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+
 	return (0);
 }
