d6c0fb19bbfa26a96d372a91f11034ee12d33bd8
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-14 02:42:24 -0800

    ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,10 +27,9 @@ void times_table(void)
 	{
 	if (y != 0)
 	{
-
 	_putchar(44);
 	_putchar(32);
-	_putchar(32)
+	_putchar(32);
 	}
 
 	_putchar(z + '0');
@@ -40,4 +39,3 @@ void times_table(void)
 	_putchar('\n');
 	}
 }
-
