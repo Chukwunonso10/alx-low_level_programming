5397dfa741c8b1c46e66167b826f50ae3ff20d9f
chukwunonso10 <kuzuechinonsojude@gmail.com>
2022-12-21 09:41:39 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncat - concatinate the values of two integers.
  * @dest: arg a
@@ -17,6 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 	i++;
 	}
+
 	j = 0;
 
 	while (src[j] != '\0' && j < n)
