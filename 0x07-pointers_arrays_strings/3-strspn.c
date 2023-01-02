7ba7383d5e7e14b47326abfb874a309305abd59d
chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-01-02 09:11:39 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,6 +11,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count1, count2;
+
 	for (count1 = 0; s[count1] != '\0'; count1++)
 	{
 	for (count2 = 0; accept[count2] != '\0'; count2++)
