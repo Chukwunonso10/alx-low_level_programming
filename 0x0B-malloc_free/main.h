80cb29ad3fbe0e3cafb8e8eca328683a91ac5f96
chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-01-09 10:10:19 -0800

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+char *create_array(unsigned int size, char c);
+void free_grid(int **grid, int height);
+
+
+
+
+#endif
