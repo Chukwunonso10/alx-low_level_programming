2bf91044c4d1fef1ab4d9635654fabbd30425e13
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-01-18 19:15:15 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,8 @@
 #include <string.h>
 
 /**
- * <<<<<<< HEAD
+ *
+<<<<<<< HEAD
  * get_op_func - ...
  * @s: ...
  *
@@ -22,11 +23,11 @@ int (*get_op_func(char *s))(int, int)
 		{ "+", op_add },
 		{ "-", op_sub },
 		{ "*", op_mul },
-	<<<<<<< HEAD
+<<<<<<< HEAD
 	{ "/", op_div }
-	=======
+=======
 	{ "/", op_div },
-		>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
 		{ "%", op_mod },
 		{ NULL, NULL }
 	};
@@ -36,11 +37,11 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-		<<<<<<< HEAD
+<<<<<<< HEAD
 
-			=======
+=======
 
-			>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
 			i++;
 	}
 
