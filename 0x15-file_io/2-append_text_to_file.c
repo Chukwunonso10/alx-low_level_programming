fab68411e55dc6f319b39dd7412110e12d8cd09a
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-02-06 13:59:08 -0800

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,54 +1,41 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _strlen - counts string length
- * @str: string to be used
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: Const char pointer
  *
- * Return: length of the string
- */
-
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
-/**
- * append_text_to_file - appends text at the end of a file
- *  @filename: name of the file
- *  @text_content: content to be appended
- *
- *  Return: 1 on success, -1 otherwise
+ * @text_content: Char pointer
+ * Return: int
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wrote;
+	int fdOpen, fdWrite, length = 0;
 
 	if (filename == NULL)
-		return (-1)
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
 		return (-1);
-	if (text_content != NULL)
-	{
-		wrote = write(file, text_content, _strlen(text_content));
-		if (wrote == -1)
-		{
-			close(file);
-			return (-1);
-		}
-		close(file);
-		return (1);
-	}
-	else
-	{
-		close(file);
-		return (1);
-	}
-}
 
+	fdOpen = open(filename, O_WRONLY | O_APPEND);
+
+	if (fdOpen == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+
+	fdWrite = write(fdOpen, text_content, length);
+
+	if (fdWrite == -1)
+		return (-1);
+
+	close(fdOpen);
+
+	return (1);
+}
