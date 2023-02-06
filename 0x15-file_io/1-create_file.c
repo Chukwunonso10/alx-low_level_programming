056ff0af5719e5029f7e054fcfb5e3086129560b
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-02-06 13:21:25 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int _strlen(char *str)
  *  Return: 1 on success, -1 otherwise
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int file, wrote;
 
