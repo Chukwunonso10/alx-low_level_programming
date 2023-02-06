db86b43b1d68569cca62cfb749f2e5168be34ce7
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-02-06 13:14:21 -0800

    under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,8 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)i;
+int _strlen(char *str);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /*MAIN_H*/
